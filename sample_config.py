329b7ce67a7258114314dbe144cc36ce9c41aa12
TeamInnexia <81477304+TeamInnexia@users.noreply.github.com>
2021-04-03 13:50:36 +0530

    Initial commit

---
+++ sample_config.py
@@ -0,0 +1,13 @@
+import os
+
+class Var(object):
+    APP_ID = int(os.environ.get("APP_ID", 6))
+    # 6 is a placeholder
+    API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+    STRING_SESSION = os.environ.get("STRING_SESSION", None)
+    DB_URI = os.environ.get("DATABASE_URL", None)
+    TG_BOT_TOKEN_BF_HER = os.environ.get("TG_BOT_TOKEN_BF_HER", None)
+    TG_BOT_USER_NAME_BF_HER = os.environ.get("TG_BOT_USER_NAME_BF_HER", None)
+    
+class Development(Var):
+    LOGGER = False
---
+++ userbot/plugins/__init__.py
@@ -0,0 +1,4 @@
+from userbot import *
+from userbot.utils import *
+from userbot.Config import Config
+
